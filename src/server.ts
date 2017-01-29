b92c52d13d7e9752a9288e23bae7e272b4d539c1
Carl Winkler <carl@winkler.id.au>
2017-01-29 11:04:09 +0000

    #3 Add 404 handler

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import * as express from 'express'
+import { StatusError } from './api/error'
 import api from './api'
 import errorHandler from './error-handler'
 
@@ -6,6 +7,13 @@ const app = express()
 
 app.use('/', api)
 app.use(express.static('front'))
+
+app.use('/', (_, __, next) => {
+  const error = new StatusError('Resource not found', 404)
+  next(error)
+  return
+})
+
 app.use(errorHandler)
 
 export default app
