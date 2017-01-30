d7505ad08d92f5184a7adc0553558e0f8cb6d659
Carl Winkler <carl@winkler.id.au>
2017-01-30 12:14:49 +0000

    Use local storage to track vote eligibility

--- front/src/components/quote/index.ts
+++ front/src/components/quote/index.ts
@@ -1,5 +1,6 @@
 import * as ko from 'knockout'
 import * as fs from 'fs'
+import * as store from '../../store'
 
 class QuoteVM {
   id = ko.observable(0)
@@ -25,12 +26,48 @@ class QuoteVM {
   }
 
   voteUp = async () => {
+    if (this.getVoteState() === Voted.Up) {
+      return
+    }
+
     await fetch(`/quote/${this.id}/up`)
+    this.votes(this.votes() + 1)
+    store.set(this.getVoteKey(), 'up')
   }
 
   voteDown = async () => {
+    if (this.getVoteState() === Voted.Down) {
+      return
+    }
+
     await fetch(`/quote/${this.id}/down`)
+    this.votes(this.votes() - 1)
+    store.set(this.getVoteKey(), 'down')
   }
+
+  getVoteState(): Voted {
+    const storedState: VoteState = store.get(`voteState_${this.id()}`)
+    switch (storedState) {
+      case 'up':
+        return Voted.Up
+      case 'down':
+        return Voted.Down
+      default:
+        return Voted.None
+    }
+  }
+
+  getVoteKey(): string {
+    return `voteState_${this.id()}`
+  }
+}
+
+type VoteState = 'up' | 'down' | null
+
+const enum Voted {
+  None,
+  Up,
+  Down
 }
 
 ko.components.register('ge-quote', {
