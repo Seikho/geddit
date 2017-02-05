a0801d8b80293db01cf51fdc51e8b4b9b370ef30
Carl Winkler <carl@winkler.id.au>
2017-02-05 08:15:48 +0000

    Send 500 errors to home-page

--- src/error-handler.ts
+++ src/error-handler.ts
@@ -1,8 +1,10 @@
 import { ErrorRequestHandler } from 'express'
 import { StatusError } from './api/error'
 import logger from './logger'
+import * as path from 'path'
 
 const isProduction = process.env.NODE_ENV === 'production'
+const staticPath = path.resolve(__dirname, '..', 'front')
 
 const handler: ErrorRequestHandler = (error: Error | StatusError | any, _, res) => {
   if (error instanceof StatusError) {
@@ -19,9 +21,11 @@ const handler: ErrorRequestHandler = (error: Error | StatusError | any, _, res) 
     const errorObject = isProduction
       ? { message: error.message }
       : { message: error.message, stack: error.stack }
+
+    console.log(JSON.stringify(errorObject, null, 2)) //tslint:disable-line
     res
       .status(500)
-      .json(errorObject)
+      .sendFile(path.resolve(staticPath, 'index.html'))
 
     logger.error(error.message)
     logger.error(error.stack)
@@ -30,7 +34,7 @@ const handler: ErrorRequestHandler = (error: Error | StatusError | any, _, res) 
 
   res
     .status(500)
-    .json({ message: 'Internal Server Error' })
+    .sendFile(path.resolve(staticPath, 'index.html'))
 
   logger.error('Unexpected error occurred:')
   logger.error(JSON.stringify(error, null, 2))
