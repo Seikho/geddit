6c330fd5624a453be4fde2af811f0c703e90c7b6
Carl Winkler <carl@winkler.id.au>
2017-02-09 11:53:36 +0000

    #15 Implement user GET route

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -5,9 +5,12 @@ import logout from './logout'
 import whoAmI from './who-am-i'
 import changePassword from './change-password'
 import register from './register'
+import getAll from './get-all'
+import paging from '../paging'
 
 const router = Router()
 
+router.get('/get-all', paging, getAll)
 router.get('/who-am-i', whoAmI)
 router.get('/logout', logout)
 router.post('/login', login)
