6e6643f892e25e94dc9d344524da72f6580f4a13
Carl Winkler <carl@carlossus.com>
2017-02-11 19:37:29 +0800

    [WIP] Implement user-list

--- front/src/components/user-list/index.ts
+++ front/src/components/user-list/index.ts
@@ -4,6 +4,13 @@ import { user } from '../../api'
 import PagerVM from '../pager'
 
 class UserListVM extends PagerVM<Schema.User> {
+  accessLevels: Array<AccessLabel> = [
+    [AccessLevel.Disabled, 'Disabled'],
+    [AccessLevel.Contributor, 'Contributor'],
+    [AccessLevel.Moderator, 'Moderator'],
+    [AccessLevel.Administrator, 'Administrator']
+  ]
+
   constructor() {
     super({
       page: 1,
@@ -11,13 +18,20 @@ class UserListVM extends PagerVM<Schema.User> {
       fetcher: (page, pageSize) => user.getMany(page, pageSize)
     })
   }
+
+  getLabel = (accessLevel: AccessLevel) => {
+    const label = this.accessLevels.find(level => level[0] === accessLevel) as AccessLabel
+    return label[1]
+  }
 }
 
 ko.components.register('ge-user-list', {
-  template: fs.readFileSync(`${__dirname}/users.html`).toString(),
+  template: fs.readFileSync(`${__dirname}/user-list.html`).toString(),
   viewModel: {
     createViewModel: () => new UserListVM()
   }
 })
 
 export default UserListVM
+
+type AccessLabel = [AccessLevel, string]
