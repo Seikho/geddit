36ceebe8f023d02b2a60f55175993abe985fa661
Carl Winkler <carl@carlossus.com>
2017-02-12 20:40:37 +0800

    Add quote soft deletion

--- src/api/quote/approve.ts
+++ src/api/quote/approve.ts
@@ -10,7 +10,12 @@ const handler: RequestHandler = async (req, res, next) => {
     const error = new StatusError('Unauthorized', 401)
     return next(error)
   }
-  
+
+  if (user.accessLevel < AccessLevel.Moderator) {
+    const error = new StatusError('Unauthorized', 401)
+    return next(error)
+  }
+
   const id = req.params.id
   const status = req.params.status
 
@@ -25,17 +30,23 @@ const handler: RequestHandler = async (req, res, next) => {
 
   if (!quote) {
     const error = new StatusError(`Quote '${id}' not found`, 404)
-    next(error)
-    return
+    return next(error)
+  }
+
+  if (quote.isDeleted && user.accessLevel !== AccessLevel.Administrator) {
+    const error = new StatusError(`Unauthorized: Only administrators can alter deleted quotes`, 401)
+    return next(error)
   }
 
   if (status === 'disallow') {
     await db(QUOTE)
-      .delete()
+      .update('isDeleted', true)
+      .update('approved', false)
       .where('id', id)
   } else {
     await db(QUOTE)
       .update('approved', true)
+      .update('isDeleted', false)
       .where('id', id)
   }
 
