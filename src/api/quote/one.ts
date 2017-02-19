b7fd4e9bdf9a9afc553921021a139b6014ed1280
Carl Winkler <carl@winkler.id.au>
2017-02-19 05:06:39 +0000

    Add GA

--- src/api/quote/one.ts
+++ src/api/quote/one.ts
@@ -52,6 +52,18 @@ function createQuoteHtml(quote: Schema.Quote) {
   <ge-body>
   </ge-body>
   <script src= "/bundle.js" defer> </script>
+  <script defer>
+    (function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+      (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date(); a = s.createElement(o),
+      m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-61186849-3', 'auto');
+    ga('send', 'pageview');
+
+  </script>
 </body>
 
 </html>`
